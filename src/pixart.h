da8057442b0343656477996412d6d7cda72fa868
semtexfromua <125829448+semtexfromua@users.noreply.github.com>
2025-04-17 16:57:25 +0300

    Update pixart.h

--- src/pixart.h
+++ src/pixart.h
@@ -15,7 +15,7 @@
 extern "C" {
 #endif
 
-enum pixart_input_mode { MOVE = 0, SCROLL, SNIPE, ARROW };
+enum pixart_input_mode { MOVE = 0, SCROLL, SNIPE };
 
 /* device data structure */
 struct pixart_data {
@@ -59,10 +59,7 @@ struct pixart_config {
     int32_t *scroll_layers;
     size_t snipe_layers_len;
     int32_t *snipe_layers;
-    size_t arrow_layers_len;
-    int32_t *arrow_layers;
 };
-
 
 #ifdef __cplusplus
 }
