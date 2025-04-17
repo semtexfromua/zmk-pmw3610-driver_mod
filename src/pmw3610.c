3d73f1859d3ab6fc26e7d7e3f0a49b20cf48437f
semtexfromua <125829448+semtexfromua@users.noreply.github.com>
2025-04-17 18:30:30 +0300

    Update pmw3610.c

--- src/pmw3610.c
+++ src/pmw3610.c
@@ -14,21 +14,18 @@
 #include <zephyr/sys/byteorder.h>
 #include <zephyr/input/input.h>
 #include <zmk/keymap.h>
-#include <zmk/behavior.h>
-#include <zmk/input.h>
 
 #include "pmw3610.h"
 
 #include <zephyr/logging/log.h>
 
 // HID keycodes для стрілок
+#define default_keypress_config ((struct keypress_config){0, false})
+
 #define KEY_RIGHT 0x4F
 #define KEY_LEFT  0x50
 #define KEY_DOWN  0x51
 #define KEY_UP    0x52
-
-extern const struct keypress_config default_keypress_config;
-
 LOG_MODULE_REGISTER(pmw3610, CONFIG_INPUT_LOG_LEVEL);
 
 //////// Sensor initialization steps definition //////////
